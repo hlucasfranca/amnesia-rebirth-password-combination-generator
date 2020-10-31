039513bf2cd41395b476f7da16218fae50be3463
Henrique Lucas França <hlucasfranca@gmail.com>
2020-10-31 20:12:59 -0300

    Adding found codes

--- generator.py
+++ generator.py
@@ -5,11 +5,13 @@ from tqdm import tqdm
 
 codes = [
     ['7ui8'],
-    ['e7g5', 'g5e7'],
     ['3an2'],
-    ['a3Y9', 'd3Y9', '43Y9'],
-    ['a101', 'a1o1', 'a10i', 'ai01'],
-    ['h154', 'h1s4']
+    ['a101', 'a1o1'],
+    ['e7g5', 'g5e7'],
+    ['j26k'],
+    ['4S3D'],
+    ['a3Y9', 'd3Y9']
+    #,    ['h154', 'h1s4']
 ]
 
 def all_casings(input_string):
@@ -26,20 +28,16 @@ def all_casings(input_string):
                 yield first.upper() + sub_casing
 
 def generate_passwords_file(words):
-    a = 0
-
     total = len(list(permutations(words)))
 
     with open('passwords.txt', 'w') as file:
         for permutacao in tqdm(total = total, iterable=permutations(words)):
             for L in range(0, len(permutacao)+1):
-                for subset in combinations(permutacao, L):            
+                for subset in combinations(permutacao, L):
                     for word in [''.join(p) for p in product(*subset)]:
-                        for password in all_casings(word):                          
-                            a +=1
+                        for password in all_casings(word):
                             file.write(password)
                             file.write('\n')
-
 
 generate_passwords_file(codes)
 
